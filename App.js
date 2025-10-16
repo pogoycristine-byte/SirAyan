26ffcf1e65c73a8adc97f9f893859f969488343d
pogoy <pogoycristine@gmail.com>
2025-10-16 09:32:22 +0800

    bro

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-//edit nalang
+//edit nalang siguro
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
